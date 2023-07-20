cfc712ecfc89f56f2959ce31a90e1f7cdff77164
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-07-20 12:38:42 +1200

    All functions in index.js file were refactored

--- index.js
+++ index.js
@@ -11,15 +11,10 @@
 // TODO: Refactor this function using the map() method to return an array of objects which have only the `id` and `temperament` properties
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 const getDogsTemperaments = (dogs) => {
-  const dogsTemperaments = [];
-
-  for (let i = 0; i < dogs.length; i++) {
-    const temperament = {
-      id: dogs[i].id,
-      temperament: dogs[i].temperament,
-    };
-    dogsTemperaments.push(temperament);
-  }
+  const dogsTemperaments = dogs.map(dog => ({
+    id: dog.id,
+    temperament: dog.temperament,
+  }))
 
   return dogsTemperaments;
 };
@@ -27,13 +22,7 @@ const getDogsTemperaments = (dogs) => {
 // TODO: Refactor this function using the filter() method to return an array of objects of dogs which are older than the supplied minAge
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 const getOldDogs = (dogs, minAge) => {
-  const oldDogs = [];
-
-  for (let i = 0; i < dogs.length; i++) {
-    if (dogs[i].age >= minAge) {
-      oldDogs.push(dogs[i]);
-    }
-  }
+  const oldDogs = dogs.filter(dog => dog.age >= minAge);
 
   return oldDogs;
 };
@@ -43,14 +32,7 @@ const getOldDogs = (dogs, minAge) => {
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
 
 const getADogByTemperament = (dogs, temperament) => {
-  let dogByTemperament;
-
-  for (let i = 0; i < dogs.length; i++) {
-    if (dogs[i].temperament.includes(temperament)) {
-      dogByTemperament = dogs[i];
-      break;
-    }
-  }
+  const dogByTemperament = dogs.find(dog => dog.temperament.includes(temperament));
 
   return dogByTemperament;
 };
@@ -59,13 +41,8 @@ const getADogByTemperament = (dogs, temperament) => {
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
 const getDogsByTemperament = (dogs, temperament) => {
-  const dogsByTemperament = [];
+  const dogsByTemperament = dogs.filter(dog => dog.temperament.includes(temperament));
 
-  for (let i = 0; i < dogs.length; i++) {
-    if (dogs[i].temperament.includes(temperament)) {
-      dogsByTemperament.push(dogs[i]);
-    }
-  }
 
   return dogsByTemperament;
 };
@@ -73,13 +50,10 @@ const getDogsByTemperament = (dogs, temperament) => {
 // TODO: Refactor this function using the reduce() method to calculate the average age of the dogs in the array
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
 const getDogsAverageAge = (dogs) => {
-  let accumulatedAge = 0;
+  
   const numberOfDogs = dogs.length;
-
-  for (let i = 0; i < numberOfDogs; i++) {
-    accumulatedAge = accumulatedAge + dogs[i].age;
-  }
-
+  const accumulatedAge = dogs.reduce((total, dog) => total + dog.age, 0);
+  
   return accumulatedAge / numberOfDogs;
 };
 
